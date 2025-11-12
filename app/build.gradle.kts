a65a9f332e8bfe88773251ac770809d581ec2adf
MauroRaya <maurorayabr@gmail.com>
2025-11-12 20:22:28 -0300

    feat: add firebase lib and base code for sign in

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.firebaseui:firebase-ui-auth:9.0.0")
 }
--- app/src/main/java/br/unisanta/consultorio/MainActivity.kt
+++ app/src/main/java/br/unisanta/consultorio/MainActivity.kt
@@ -1,20 +1,79 @@
-package br.unisanta.consultorio
+package br.unisanta.appfirebase
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract(),
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+//        val btnSignOut = findViewById<FloatingActionButton>(R.id.btn_sign_out)
+//
+//        btnSignOut.setOnClickListener {
+//            signOut()
+//            createSignInIntent()
+//        }
+
+        createSignInIntent()
+    }
+
+    private fun createSignInIntent() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+            AuthUI.IdpConfig.AnonymousBuilder().build()
+        )
+
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+
+        if (result.resultCode == RESULT_OK) {
+            // val user = FirebaseAuth.getInstance().currentUser
+
+            Toast.makeText(
+                this,
+                "Login bem sucedido",
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            if (response == null) {
+                Toast.makeText(
+                    this,
+                    "Login cancelado",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Erro ao realizar login",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
+    }
+
+    private fun signOut() {
+        AuthUI.getInstance()
+            .signOut(this)
     }
 }

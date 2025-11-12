30b5683dce7ec23d0d8596844c1825718daa5a1e
MauroRaya <maurorayabr@gmail.com>
2025-11-12 20:39:09 -0300

    feat and fix: add sign out and switch activity buttons and fix activity_main.xml display

--- app/src/main/java/br/unisanta/consultorio/MainActivity.kt
+++ app/src/main/java/br/unisanta/consultorio/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
@@ -19,13 +21,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
-//        val btnSignOut = findViewById<FloatingActionButton>(R.id.btn_sign_out)
-//
-//        btnSignOut.setOnClickListener {
-//            signOut()
-//            createSignInIntent()
-//        }
+        val btnSignOut = findViewById<FloatingActionButton>(R.id.fab_sign_out)
+
+        btnSignOut.setOnClickListener {
+            signOut()
+            createSignInIntent()
+        }
 
         createSignInIntent()
     }
